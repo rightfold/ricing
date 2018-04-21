09505041383bc283fbf120c49a61b18baed8c38c
rightfold <rightfold@gmail.com>
2018-04-22 01:12:41 +0200

    Rice

--- Update.sh
+++ Update.sh
@@ -14,6 +14,7 @@ fi
 # Clean up the repository before repopulating it.
 rm -rf "$PWD/etc"
 rm -rf "$PWD/home"
+rm -rf "$PWD/var"
 
 # Copy the sudoers file using cat. This is explicitly allowed without password
 # through a line in the sudoers file.
@@ -23,13 +24,21 @@ sudo cat /etc/sudoers > "$PWD/etc/sudoers"
 # Copy the Portage configuration.
 mkdir -p "$PWD/etc/portage"
 mkdir -p "$PWD/etc/portage/repos.conf"
+mkdir -p "$PWD/var/lib/portage"
 cp /etc/portage/{make.conf,package.{accept_keywords,mask,use}} "$PWD/etc/portage"
 cp /etc/portage/repos.conf/rightfold-overlay.conf "$PWD/etc/portage/repos.conf"
+cp /var/lib/portage/world "$PWD/var/lib/portage"
 
 # Copy the X configuration.
 mkdir -p "$PWD/home/r"
 cp /home/r/.xinitrc "$PWD/home/r"
 cp /home/r/.Xresources "$PWD/home/r"
+
+# Copy the i3 configuration.
+mkdir -p "$PWD/home/r/.config/i3"
+mkdir -p "$PWD/home/r/.config/i3status"
+cp /home/r/.config/i3/config "$PWD/home/r/.config/i3"
+cp /home/r/.config/i3status/config "$PWD/home/r/.config/i3status"
 
 # Copy the Nginx configuration.
 mkdir -p "$PWD/etc/nginx"
